7334710cdfa1d2d94c4ab37a1623a026fcda3f14
ono-78 <filijose00@gmail.com>
2026-03-03 11:16:25 -0400

    Add Televisor class with properties and constructor

---
+++ Ejercicio 2/Televisor.java
@@ -0,0 +1,16 @@
+public class Televisor {
+
+    private String marca;
+    private int resolucion;
+    private String tipo;
+
+    public Televisor(String marca, int resolucion, String tipo) {
+        this.marca = marca;
+        this.resolucion = resolucion;
+        this.tipo = tipo;
+    }
+
+    public String toString() {
+        return marca + " - " + resolucion + " - " + tipo;
+    }
+}
